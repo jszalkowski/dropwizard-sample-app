41a885e0833a2b5a934ccda07852a290b0cf7eea
JoeBew42 <joebew42@gmail.com>
2016-03-22 16:52:12 +0100

    added integration test for jenkins

--- cookbooks/ci/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/ci/test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,8 @@ describe 'ci::default' do
   describe command('/usr/bin/java -version') do
     its(:stderr) { should contain('1.7') }
   end
+
+  describe port(8080) do
+    it { should be_listening }
+  end
 end
