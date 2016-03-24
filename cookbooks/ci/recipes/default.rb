f653b074ac9bb669f91f4542503f5b1ff00fbf0c
JoeBew42 <joebew42@gmail.com>
2016-03-24 15:34:00 +0100

    Adding python and deployer private_key

--- cookbooks/ci/recipes/default.rb
+++ cookbooks/ci/recipes/default.rb
@@ -6,11 +6,14 @@ jenkins_plugin 'git'
 jenkins_plugin 'greenballs'
 jenkins_plugin 'junit'
 jenkins_plugin 'jobConfigHistory'
-jenkins_plugin 'delivery-pipeline-plugin' do
+jenkins_plugin 'delivery-pipeline-plugin'
+jenkins_plugin 'shiningpanda' do
   notifies :restart, 'service[jenkins]', :delayed
 end
 
 package 'git'
+package 'python-virtualenv'
+package 'python-dev'
 
 mysql_service 'test' do
   port '3306'
@@ -30,4 +33,17 @@ mysql_database 'db_notes_test' do
     :password => 'root'
   )
   action :create
+end
+
+directory "#{node['jenkins']['master']['home']}/.ssh" do
+  owner node['jenkins']['master']['user']
+  group node['jenkins']['master']['group']
+  mode '0700'
+end
+
+remote_file "#{node['jenkins']['master']['home']}/.ssh/deployer" do
+  source 'https://gist.githubusercontent.com/joebew42/440c14b70ee305af31f6/raw/2ccd359966d523a026123a434dba262ca9a90e79/deployer'
+  owner node['jenkins']['master']['user']
+  group node['jenkins']['master']['group']
+  mode '0600'
 end
